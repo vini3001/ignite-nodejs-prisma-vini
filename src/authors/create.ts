e3b1e17e6216179dc72e277b90ea7808ad03c0dd
vini3001 <donizetevinicius250@gmail.com>
2022-07-19 15:14:51 -0300

    feito

---
+++ src/authors/create.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.authors.create({
+    data: {
+      name: "Eric Evans ",
+    },
+  });
+
+  console.log(result);
+}
